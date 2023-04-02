b0e4154670c1f0d2456eda444d2d90c07d5d9c10
miao.zhang <miao.zhang@thoughtworks.com>
2023-04-02 21:08:10 +0800

    feat:add test should return empty list with query no value

--- src/pages/Home/components/BoardMain/BoardMain.spec.tsx
+++ src/pages/Home/components/BoardMain/BoardMain.spec.tsx
@@ -5,6 +5,7 @@ import { StoreWrapper } from '../../../../Utils/wrapperTest';
 import '@testing-library/jest-dom';
 import { renderHook } from '@testing-library/react-hooks';
 import { useCardList } from './hooks';
+import { act } from '@testing-library/react-hooks/native';
 
 
 describe('BoardMain cardList', function() {
@@ -19,6 +20,12 @@ describe('BoardMain cardList', function() {
 
 	it('should default empty list',function(){
 		const { result } = renderHook(() => useCardList(),{wrapper:StoreWrapper});
+		expect(result.current.list).toEqual([]);
+	});
+
+	it('should return empty list with query no value', function() {
+		const {result}=renderHook(()=>useCardList(),{wrapper:StoreWrapper});
+		act(()=>{result.current.onQuery('');});
 		expect(result.current.list).toEqual([]);
 	});
 });
--- src/pages/Home/components/BoardSideRight/hooks.ts
+++
@@ -1,9 +0,0 @@
-import { updateQuery } from '../../../../Slice/cardSlice';
-import { useStoreDispatch } from '../../../../Store/Store';
-
-export const useSideRight = () => {
-	const dispatch = useStoreDispatch();
-	return (value: string) => {
-		dispatch(updateQuery(value));
-	};
-};
