09ffff5b11a562050c70168d60cb9f879c51ece1
miao.zhang <miao.zhang@thoughtworks.com>
2023-04-20 09:53:25 +0800

    test: cardSlice reducer test

--- src/Slice/cardSlice.spec.ts
+++ src/Slice/cardSlice.spec.ts
@@ -77,35 +77,71 @@ describe('cardSlice test', function () {
 		expect(mockConfirmCreat.cardList.length).toEqual(2);
 	});
 	it('should activeUpdate', function () {
-		const mockConfirmUpdate = cardSlice.reducer(
+		const mockActiveUpdate = cardSlice.reducer(
 			initialCardState,
 			activeUpdate('1')
 		);
-		expect(mockConfirmUpdate.editing.id).toEqual('1');
+		expect(mockActiveUpdate.editing.id).toEqual('1');
 	});
-	// it('should confirmUpdate', function () {
-	// 	const mockConfirmUpdate = cardSlice.reducer(
-	// 		initialCardState,
-	// 		confirmUpdate({
-	// 			id: '1',
-	// 			title: '2',
-	// 			description: '2',
-	// 			rate: 2,
-	// 			updateAt: '2',
-	// 			userName: '2',
-	// 		})
-	// 	);
-	// 	expect(mockConfirmUpdate.cardList).toBe([
-	// 		{
-	// 			id: '1',
-	// 			title: '2',
-	// 			description: '2',
-	// 			rate: 2,
-	// 			updateAt: '2',
-	// 			userName: '2',
-	// 		},
-	// 	]);
-	// });
+	it('should confirmUpdate', function () {
+		const mockConfirmUpdate = cardSlice.reducer(
+			initialCardState,
+			confirmUpdate({
+				id: '1',
+				title: '2',
+				description: '2',
+				rate: 2,
+				updateAt: '2',
+				userName: '2',
+			})
+		);
+		expect(mockConfirmUpdate.cardList).toStrictEqual([
+			{
+				id: '1',
+				title: '2',
+				description: '2',
+				rate: 2,
+				updateAt: '2',
+				userName: '2',
+			},
+		]);
+	});
+
+	it('should handle confirmUpdate', () => {
+		const initialState = {
+			...initialCardState,
+			cardList: [
+				{
+					id: '1',
+					title: 'card 1',
+					description: '',
+					rate: 3,
+					updateAt: '',
+					userName: null,
+				},
+				{
+					id: '2',
+					title: 'card 2',
+					description: '',
+					rate: 4,
+					updateAt: '',
+					userName: null,
+				},
+			],
+		};
+		const updatedCard = {
+			id: '1',
+			title: 'card 1 updated',
+			description: '',
+			rate: 3,
+			updateAt: '',
+			userName: null,
+		};
+		const action = confirmUpdate(updatedCard);
+		const newState = cardSlice.reducer(initialState, action);
+		expect(newState.cardList).toContainEqual(updatedCard);
+	});
+
 	it('should confirmDelete', function () {
 		const mockConfirmDelete = cardSlice.reducer(
 			initialCardState,
